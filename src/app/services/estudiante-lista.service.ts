587a000c95a60835486a3279dae2a5bd9472b012
Matias Bordon <matzaia2001@gmail.com>
2023-11-09 18:54:56 -0300

    first commit

---
+++ src/app/services/estudiante-lista.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Students } from '../models/students';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EstudianteListaService {
+
+  private url = "https://backend-idra-production.up.railway.app/student"
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable <any>{
+    return this.http.get(this.url + '/getAll')
+  }
+
+  save(s: Students): Observable<any> {
+    return this.http.post(this.url, s);
+  }
+
+ 
+  update(s: Students): Observable<any> {
+    return this.http.post(this.url + '/' + s.id + '/update', s);
+  }
+
+  delete(/*s*/id: number): Observable <any>{
+    return 	this.http.post(this.url + '/' + /*s.*/id + '/delete', null)
+  }
+
+  /*view(id: number, Students: any): Observable <any>{
+    return this.http.post(this.url + '/' + id + '/view', null)
+  }*/
+
+}
